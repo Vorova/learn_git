83fbc9f056f29cef8361d83a4c012e13f7c39b72
Vladislav Vorobyov <vvvorova@gmail.com>
2023-04-04 17:57:48 +0500

    Change file 3 (amend)

--- File3.java
+++ File3.java
@@ -1,7 +1,8 @@
 public class File3 {
-
+	
 	public static void main(String[].. args) {
 		System.out.println("Hello, git!");
-	}	
-
+		System.out.println("It is changed file!");
+		System.out.println("It is changed file with function --amend");
+	}
 }
