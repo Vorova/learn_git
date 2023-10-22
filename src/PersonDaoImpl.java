7147d1c02fbb862f5c9ff22ad27ff233ad753cb3
vvvorova <vvvorova@gmail.com>
2023-10-22 23:21:28 +0500

    create PersonDaoImpl.java

---
+++ src/PersonDaoImpl.java
@@ -0,0 +1,13 @@
+public class PersonDaoImpl implements PersonDao {
+
+    public Long create(Person person) {
+        // todo implements method
+        return null;
+    }
+
+    public Person findById(Long id) {
+        // todo implements method
+        return null;
+    }
+
+}
