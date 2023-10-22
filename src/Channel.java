d854e486679f0e50e72dc27442d89cdb601aeb93
vvvorova <vvvorova@gmail.com>
2023-10-23 00:08:22 +0500

    add javadoc for Channel

--- src/Channel.java
+++ src/Channel.java
@@ -1,6 +1,9 @@
 
 public class Channel implements Runnable {
 
+    /**
+     * Add javadoc for this method
+     */
     @Override
     public void run() {
         System.out.println("Thread name: " + Thread.currentThread().getName());
